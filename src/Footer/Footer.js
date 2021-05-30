b644685a39705ec7f224259773e07e7801911cd2
Davide Pastore <pasdavide@gmail.com>
2021-05-30 17:15:51 +0200

    Additional fix

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
       <div className='p-4 p-md-5 text-white rounded bg-dark'>
         <div className='col-md-12 px-0'>
           <p className='lead my-3 text-center'>
-            Made with ❤ by <a href='https://github.com/DavidePastore' target='_blank' rel='noreferrer' className='text-white font-weight-bold'>Davide Pastore</a> and <a href='https://github.com/MarcoDiSalvo90' target='_blank' rel='noreferrer' className='text-white font-weight-bold'>Marco Di Salvo</a>
+            Made with ❤ by <a href='https://github.com/DavidePastore' target='_blank' rel='noopener noreferrer' className='text-white font-weight-bold'>Davide Pastore</a> and <a href='https://github.com/MarcoDiSalvo90' target='_blank' rel='noopener noreferrer' className='text-white font-weight-bold'>Marco Di Salvo</a>
           </p>
         </div>
       </div>
